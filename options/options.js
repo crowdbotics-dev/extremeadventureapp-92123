d67ee44265b76acdce48641d85cfb1a5e11f2fb7
Crowdbotics <engineering@crowdbotics.com>
2023-06-16 11:25:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "extremeadventureapp-92123",
+  url: "https://extremeadventureapp-92123.botics.co",
+  api: "https://extremeadventureapp-92123.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
